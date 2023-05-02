9145ab56d67cf7b4f1e8ffd0c783f7075b24e533
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-05-02 19:58:54 -0300

    ingredients programmed in shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,21 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
+import { Ingredient } from '../shared/ingedient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('The Test Recipe', 'A simple description', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'),
-    new Recipe('A Test Recipe', 'A simple description', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+    new Recipe('Grand snitzel', 'Delicious snitzel', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+     [
+      new Ingredient('Meat', 1),
+      new Ingredient('French fries', 20)
+     ]),
+    new Recipe('Big Borgir', 'A Big Fat Juicy Burger', 'https://static.vecteezy.com/system/resources/previews/020/640/042/original/delicious-mouth-watering-hamburger-cheeseburger-with-cheese-tomato-and-lettuce-on-a-transparent-background-free-png.png',
+     [
+      new Ingredient('buns', 2),
+      new Ingredient('Meat', 1)
+     ])
   ];
 
   getRecipes() {
