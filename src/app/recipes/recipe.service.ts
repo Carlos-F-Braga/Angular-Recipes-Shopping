b708e1754fa30b603373bb4f32353e6c04629ba9
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-05-02 20:11:06 -0300

    adding ingredients to shopping list programmed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from "./recipe.model";
 import { Ingredient } from '../shared/ingedient.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -18,7 +20,14 @@ export class RecipeService {
      ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
   }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
 }
