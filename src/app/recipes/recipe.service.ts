2af8dc1f168afc5c41b8411e70c3e8ee1742b4e8
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-05-14 16:12:12 -0300

    recipe service updated

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,26 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Grand snitzel', 'Delicious snitzel', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
-     [
-      new Ingredient('Meat', 1),
-      new Ingredient('French fries', 20)
-     ]),
-    new Recipe('Big Borgir', 'A Big Fat Juicy Burger', 'https://static.vecteezy.com/system/resources/previews/020/640/042/original/delicious-mouth-watering-hamburger-cheeseburger-with-cheese-tomato-and-lettuce-on-a-transparent-background-free-png.png',
-     [
-      new Ingredient('buns', 2),
-      new Ingredient('Meat', 1)
-     ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Grand snitzel', 'Delicious snitzel', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1200px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+  //    [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('French fries', 20)
+  //    ]),
+  //   new Recipe('Big Borgir', 'A Big Fat Juicy Burger', 'https://static.vecteezy.com/system/resources/previews/020/640/042/original/delicious-mouth-watering-hamburger-cheeseburger-with-cheese-tomato-and-lettuce-on-a-transparent-background-free-png.png',
+  //    [
+  //     new Ingredient('buns', 2),
+  //     new Ingredient('Meat', 1)
+  //    ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice())
+  }
 
   getRecipes() {
     return this.recipes.slice();
