d457da4bd68b0ff02e6c06ef2d1572d6258c4083
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-05-14 15:26:15 -0300

    space fix

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -24,7 +24,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-      this.subscription.unsubscribe()
+    this.subscription.unsubscribe()
   }
 
   onEditItem(index: number) {
