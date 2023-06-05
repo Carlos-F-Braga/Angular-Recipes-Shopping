9ab2c00903bf41eb3ffbc48c8ede3e086a911590
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-06-05 16:30:13 -0300

    auth Component implementation and programming

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthComponent } from './auth/auth/auth.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -24,7 +25,8 @@ const appRoutes: Routes = [
       resolve: [RecipesResolverService]
     }
   ] },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent }
 ]
 
 
